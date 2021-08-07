39b5cccd8524cbe2237da2a5533b50df630646f7
poseidon <jsy1030@chollian.net>
2021-08-07 15:28:48 +0900

    Widget List

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  List<String> list=List.generate(50, (index) => "Item : $index");
+  List<String> list = List.generate(50, (index) => "Item : $index");
+  List<Widget> _widgetList = [];
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < list.length; i++) {
+      _widgetList.add(Text("othe item : ${list[i]}"));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,13 +26,9 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
-        body: ListView.builder(itemBuilder: (context,index){
-          return ListTile(
-            title: Text(list[index]),
-            leading:Icon(Icons.add),
-            trailing: Icon(Icons.delete),
-          );
-        }),
+        body: ListView(
+          children: _widgetList,
+        )
       ),
     );
   }
